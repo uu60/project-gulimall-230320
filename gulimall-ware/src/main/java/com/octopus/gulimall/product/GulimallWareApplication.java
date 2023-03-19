9c7123bc9e14e1145e31c5ef21d415c2abfdbcb6
uu60 <djz6660@icloud.com>
2023-03-20 01:37:59 +0800

    添加maven3.6.3到dep目录使用，引入renren-generator并完成配置

---
+++ gulimall-ware/src/main/java/com/octopus/gulimall/product/GulimallWareApplication.java
@@ -0,0 +1,13 @@
+package com.octopus.gulimall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallWareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GulimallWareApplication.class, args);
+	}
+
+}
