f04b0c6d6407680144e4868d5fc478349ba1a112
uu60 <djz6660@icloud.com>
2023-03-20 20:51:01 +0800

    注册renren-fast到nacos，去除跨域配置

--- gulimall-common/src/main/java/com/octopus/common/utils/Query.java
+++ gulimall-common/src/main/java/com/octopus/common/utils/Query.java
@@ -10,9 +10,9 @@ package com.octopus.common.utils;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.metadata.OrderItem;
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.octopus.common.xss.SQLFilter;
+import org.apache.commons.lang.StringUtils;
 
 import java.util.Map;
 
