564c944a947d79c6fde1594d5ca71b92a61c25fd
uu60 <djz6660@icloud.com>
2023-03-31 18:52:32 +0800

    认证模块创建

---
+++ gulimall-auth-server/src/test/java/com/octopus/gulimall/GulimallAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octopus.gulimall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
